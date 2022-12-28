0d975c1f5dab1ee3f3af41854605a242f4ce5c94
Elton Peter <peterelton8@gmail.com>
2022-12-27 20:30:38 -0500

    Update Program.cs

--- CsharpCodility/Iterations/Program.cs
+++ CsharpCodility/Iterations/Program.cs
@@ -4,7 +4,77 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = 1162;
+
+            Console.WriteLine(gapFinder(n));
         }
+
+        static int gapFinder(int n)
+        {
+            int count = 0;
+            var binaryArray = Convert.ToString(n, 2).ToCharArray().Select((x, y) => new { Index = y, 
+                Value = x }).ToList();
+            bool hitFlag = false;
+            List<int> counts = new List<int>();
+            foreach (var obj in binaryArray)
+            {
+                if (obj.Value == 49)
+                {
+                    if (obj.Index != binaryArray.Count - 1)
+                    {
+                        if (!hitFlag)
+                        {
+                            if (binaryArray[obj.Index + 1].Value == 48)
+                            {
+                                hitFlag = true;
+                                count = 0;
+                            }
+                        }
+                        else
+                        {
+                            counts.Add(count);
+                            if (binaryArray[obj.Index + 1].Value == 48)
+                            {
+                                hitFlag = true;
+                                count = 0;
+                            }
+                            else
+                            {
+                                hitFlag= false;
+                            }
+                        }
+                    }
+                    else if (obj.Index == binaryArray.Count - 1)
+                    {
+                        if (hitFlag)
+                        {
+                            counts.Add(count);
+                            hitFlag = false;
+                        }
+                    }
+                }
+                else
+                {
+                    if (hitFlag)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        if (obj.Index != 0)
+                        {
+                            if (binaryArray[obj.Index - 1].Value == 48)
+                            {
+                                hitFlag = true;
+                                count++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return counts.Count != 0 ? counts.Max() : 0;
+        }
+
     }
 }
